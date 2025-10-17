8eabad2590cae36bf7f5de43ee2d64f8b4d8a856
rodrigo thoma <rodrigothdasilva@gmail.com>
2025-10-17 14:20:04 -0300

    Comentários para JavaDoc de Deliberação

--- TP1_POO/src/main/java/estudantes/entidades/Deliberacao.java
+++ TP1_POO/src/main/java/estudantes/entidades/Deliberacao.java
@@ -4,43 +4,76 @@ import professor.entidades.CodigoCurso;
 
 import java.util.Objects;
 
-/*
+/**
+ * Representa uma deliberação, que é um tipo de documento administrativo. Uma
+ * deliberação formaliza uma decisão ou resolução de um órgão colegiado.
+ *
  * @author Rodrigo Thoma da Silva
  * @author Fade Hassan Husein Kanaan
  */
 public class Deliberacao extends DocumentoAdministrativo {
+
     private String texto;
 
+    /**
+     * Construtor da classe Deliberacao.
+     *
+     * @param criador O nome do criador do documento.
+     * @param codigoCurso O código do curso associado ao documento.
+     * @param paginas O número de páginas do documento.
+     * @param texto O conteúdo textual da deliberação.
+     */
     public Deliberacao(String criador, CodigoCurso codigoCurso, int paginas, String texto) {
         super(criador, codigoCurso, paginas);
         this.texto = texto;
     }
 
-
+    /**
+     * Retorna o texto da deliberação.
+     *
+     * @return o conteúdo da deliberação.
+     */
     public String getTexto() {
         return texto;
     }
 
+    /**
+     * Compara este objeto Deliberacao com outro objeto para verificar se são
+     * iguais.
+     *
+     * @param o O objeto a ser comparado.
+     * @return true se os objetos forem iguais, false caso contrário.
+     */
     @Override
     public boolean equals(Object o) {
-            if (this == o)
-                return true;
+        if (this == o) {
+            return true;
+        }
 
-            if (o == null)
-                return false;
-
-            if (this.getClass() != o.getClass())
-                return false;
-
-            if (!super.equals(o))
-                return false;
-
-            Deliberacao deliberacao = (Deliberacao) o;
-            if (deliberacao.texto.equals(this.texto))
-                return true;
+        if (o == null) {
             return false;
+        }
+
+        if (this.getClass() != o.getClass()) {
+            return false;
+        }
+
+        if (!super.equals(o)) {
+            return false;
+        }
+
+        Deliberacao deliberacao = (Deliberacao) o;
+        if (deliberacao.texto.equals(this.texto)) {
+            return true;
+        }
+        return false;
     }
 
+    /**
+     * Retorna o código de hash para este objeto Deliberacao.
+     *
+     * @return o código de hash.
+     */
     @Override
     public int hashCode() {
         return Objects.hash(super.hashCode(), texto);
