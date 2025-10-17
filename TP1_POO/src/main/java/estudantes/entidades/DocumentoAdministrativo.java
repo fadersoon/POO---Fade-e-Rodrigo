ec1fa403072736b22aaf434f8be8e887c26aede2
rodrigo thoma <rodrigothdasilva@gmail.com>
2025-10-17 14:31:35 -0300

    Comentários para JavaDoc de DocumentoAdministrativo

--- TP1_POO/src/main/java/estudantes/entidades/DocumentoAdministrativo.java
+++ TP1_POO/src/main/java/estudantes/entidades/DocumentoAdministrativo.java
@@ -4,16 +4,35 @@ import professor.entidades.CodigoCurso;
 
 import java.util.Objects;
 
-/*
+/**
+ * Representa um documento administrativo, uma especialização da classe
+ * Documento. Esta classe serve como base para documentos que não possuem um
+ * caráter acadêmico e não necessitam de um código de autenticação.
+ *
  * @author Rodrigo Thoma da Silva
  * @author Fade Hassan Husein Kanaan
  */
 public class DocumentoAdministrativo extends Documento {
 
+    /**
+     * Construtor da classe DocumentoAdministrativo.
+     *
+     * @param criador O nome do criador do documento.
+     * @param codigoCurso O código do curso associado ao documento.
+     * @param paginas O número de páginas do documento.
+     */
     public DocumentoAdministrativo(String criador, CodigoCurso codigoCurso, int paginas) {
         super(criador, codigoCurso, paginas);
     }
 
+    /**
+     * Compara este objeto DocumentoAdministrativo com outro objeto para
+     * verificar se são iguais. A comparação é feita com base nos atributos
+     * herdados da classe Documento.
+     *
+     * @param o O objeto a ser comparado.
+     * @return true se os objetos forem iguais, false caso contrário.
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) {
@@ -31,6 +50,11 @@ public class DocumentoAdministrativo extends Documento {
         return true;
     }
 
+    /**
+     * Retorna o código de hash para este objeto DocumentoAdministrativo.
+     *
+     * @return o código de hash.
+     */
     @Override
     public int hashCode() {
         return Objects.hash(super.hashCode());
