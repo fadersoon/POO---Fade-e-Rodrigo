c21868e38e611c277e28902811e4b0d8f728a7f2
rodrigo thoma <rodrigothdasilva@gmail.com>
2025-10-17 14:52:35 -0300

    Comentários para JavaDoc de Registro

--- TP1_POO/src/main/java/estudantes/entidades/Registro.java
+++ TP1_POO/src/main/java/estudantes/entidades/Registro.java
@@ -4,7 +4,11 @@ import professor.entidades.CodigoCurso;
 
 import java.util.Objects;
 
-/*
+/**
+ * Representa um registro acadêmico associado a um estudante específico.
+ * Esta classe é uma especialização de DocumentoAcademico e serve como base
+ * para documentos como históricos, certificados e atestados.
+ *
  * @author Rodrigo Thoma da Silva
  * @author Fade Hassan Husein Kanaan
  */
@@ -12,20 +16,46 @@ public class Registro extends DocumentoAcademico {
     private String estudante;
     private long matricula;
 
+    /**
+     * Construtor da classe Registro.
+     *
+     * @param criador       O nome do criador do documento.
+     * @param codigoCurso   O código do curso associado ao documento.
+     * @param paginas       O número de páginas do documento.
+     * @param autenticacao  O código de autenticação do documento.
+     * @param estudante     O nome do estudante a quem o registro pertence.
+     * @param matricula     O número de matrícula do estudante.
+     */
     public Registro(String criador, CodigoCurso codigoCurso, int paginas, long autenticacao, String estudante, long matricula) {
         super(criador, codigoCurso, paginas, autenticacao);
         this.estudante = estudante;
         this.matricula = matricula;
     }
 
+    /**
+     * Retorna o nome do estudante.
+     *
+     * @return o nome do estudante.
+     */
     public String getEstudante() {
         return estudante;
     }
 
+    /**
+     * Retorna o número de matrícula do estudante.
+     *
+     * @return a matrícula do estudante.
+     */
     public long getMatricula() {
         return matricula;
     }
 
+    /**
+     * Compara este objeto Registro com outro objeto para verificar se são iguais.
+     *
+     * @param o O objeto a ser comparado.
+     * @return true se os objetos forem iguais, false caso contrário.
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o)
@@ -46,6 +76,11 @@ public class Registro extends DocumentoAcademico {
         return false;
     }
 
+    /**
+     * Retorna o código de hash para este objeto Registro.
+     *
+     * @return o código de hash.
+     */
     @Override
     public int hashCode() {
         return Objects.hash(super.hashCode(), estudante, matricula);
