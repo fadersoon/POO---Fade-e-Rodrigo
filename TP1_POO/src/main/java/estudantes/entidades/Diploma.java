46dd0d68161ec57aa1016e90704ebd02709b459f
rodrigo thoma <rodrigothdasilva@gmail.com>
2025-10-17 14:22:44 -0300

    Comentários para JavaDoc de Diploma

--- TP1_POO/src/main/java/estudantes/entidades/Diploma.java
+++ TP1_POO/src/main/java/estudantes/entidades/Diploma.java
@@ -4,44 +4,82 @@ import professor.entidades.CodigoCurso;
 
 import java.util.Objects;
 
-/*
+/**
+ * Representa um diploma, que é um tipo específico de certificado. O diploma
+ * atesta a conclusão de um curso e confere uma habilitação ao estudante.
+ *
  * @author Rodrigo Thoma da Silva
  * @author Fade Hassan Husein Kanaan
  */
 public class Diploma extends Certificado {
+
     private String habilitacao;
 
-    public Diploma(String criador, CodigoCurso codigoCurso, int paginas,  long matricula, String descricao, long autenticacao, String estudante,
-                   String habilitacao) {
+    /**
+     * Construtor da classe Diploma.
+     *
+     * @param criador O nome do criador do documento.
+     * @param codigoCurso O código do curso associado ao documento.
+     * @param paginas O número de páginas do documento.
+     * @param matricula A matrícula do estudante.
+     * @param descricao A descrição do diploma.
+     * @param autenticacao O código de autenticação do registro.
+     * @param estudante O nome do estudante a quem o diploma se refere.
+     * @param habilitacao A habilitação conferida pelo diploma (ex: Bacharel em
+     * Engenharia de Software).
+     */
+    public Diploma(String criador, CodigoCurso codigoCurso, int paginas, long matricula, String descricao, long autenticacao, String estudante,
+            String habilitacao) {
         super(criador, codigoCurso, paginas, autenticacao, estudante, matricula, descricao);
         this.habilitacao = habilitacao;
     }
 
-
+    /**
+     * Retorna a habilitação conferida pelo diploma.
+     *
+     * @return a habilitação do diploma.
+     */
     public String getHabilitacao() {
         return habilitacao;
     }
 
+    /**
+     * Compara este objeto Diploma com outro objeto para verificar se são
+     * iguais.
+     *
+     * @param o O objeto a ser comparado.
+     * @return true se os objetos forem iguais, false caso contrário.
+     */
     @Override
     public boolean equals(Object o) {
-        if (this == o)
+        if (this == o) {
             return true;
+        }
 
-        if (this == null)
+        if (this == null) {
             return false;
+        }
 
-        if (this.getClass() != o.getClass())
+        if (this.getClass() != o.getClass()) {
             return false;
+        }
 
-        if (!super.equals(o))
+        if (!super.equals(o)) {
             return false;
+        }
 
         Diploma diploma = (Diploma) o;
-        if (diploma.habilitacao.equals(this.habilitacao))
+        if (diploma.habilitacao.equals(this.habilitacao)) {
             return true;
+        }
         return false;
     }
 
+    /**
+     * Retorna o código de hash para este objeto Diploma.
+     *
+     * @return o código de hash.
+     */
     @Override
     public int hashCode() {
         return Objects.hash(super.hashCode(), habilitacao);
