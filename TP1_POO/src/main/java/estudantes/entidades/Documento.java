5d54fe5718dbbe8b46e31dab83c33064a84e728d
rodrigo thoma <rodrigothdasilva@gmail.com>
2025-10-17 14:26:04 -0300

    Comentários para JavaDoc de Documento

--- TP1_POO/src/main/java/estudantes/entidades/Documento.java
+++ TP1_POO/src/main/java/estudantes/entidades/Documento.java
@@ -4,8 +4,11 @@ import professor.entidades.CodigoCurso;
 
 import java.util.Objects;
 
-/*
- * Classe que representa um documento genérico.
+/**
+ * Representa um documento genérico dentro do sistema da universidade. Esta é
+ * uma classe abstrata que serve como base para todos os outros tipos de
+ * documentos.
+ *
  * @author Rodrigo Thoma da Silva
  * @author Fade Hassan Husein Kanaan
  */
@@ -15,25 +18,54 @@ public abstract class Documento {
     private CodigoCurso codigoCurso;
     private int paginas;
 
+    /**
+     * Construtor da classe abstrata Documento.
+     *
+     * @param criador O nome do criador do documento.
+     * @param codigoCurso O código do curso ao qual o documento pertence.
+     * @param paginas O número de páginas do documento.
+     */
     public Documento(String criador, CodigoCurso codigoCurso, int paginas) {
         this.criador = criador;
         this.codigoCurso = codigoCurso;
         this.paginas = paginas;
     }
 
-
+    /**
+     * Retorna o nome do criador do documento.
+     *
+     * @return o nome do criador.
+     */
     public String getCriador() {
         return criador;
     }
 
+    /**
+     * Retorna o código do curso associado ao documento.
+     *
+     * @return o código do curso.
+     */
     public CodigoCurso getCodigoCurso() {
         return codigoCurso;
     }
 
+    /**
+     * Retorna o número de páginas do documento.
+     *
+     * @return o número de páginas.
+     */
     public int getPaginas() {
         return paginas;
     }
 
+    /**
+     * Compara este objeto Documento com outro objeto para verificar se são
+     * iguais. A comparação é baseada no criador, código do curso e número de
+     * páginas.
+     *
+     * @param o O objeto a ser comparado.
+     * @return true se os objetos forem iguais, false caso contrário.
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) {
@@ -53,6 +85,11 @@ public abstract class Documento {
         return false;
     }
 
+    /**
+     * Retorna o código de hash para este objeto Documento.
+     *
+     * @return o código de hash.
+     */
     @Override
     public int hashCode() {
         return Objects.hash(criador, codigoCurso, paginas);
