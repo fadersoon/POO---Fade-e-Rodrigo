47a1d8555851aa9d229198d8d397a151abd7126b
rodrigo thoma <rodrigothdasilva@gmail.com>
2025-10-17 14:28:46 -0300

    Comentários para JavaDoc de DocumentoAcademico

--- TP1_POO/src/main/java/estudantes/entidades/DocumentoAcademico.java
+++ TP1_POO/src/main/java/estudantes/entidades/DocumentoAcademico.java
@@ -4,22 +4,47 @@ import professor.entidades.CodigoCurso;
 
 import java.util.Objects;
 
-/*
+/**
+ * Representa um documento acadêmico, que é uma especialização de um Documento.
+ * Caracteriza-se por possuir um código de autenticação para validar sua
+ * veracidade.
+ *
  * @author Rodrigo Thoma da Silva
  * @author Fade Hassan Husein Kanaan
  */
 public class DocumentoAcademico extends Documento {
+
     private long autenticacao;
 
+    /**
+     * Construtor da classe DocumentoAcademico.
+     *
+     * @param criador O nome do criador do documento.
+     * @param codigoCurso O código do curso associado ao documento.
+     * @param paginas O número de páginas do documento.
+     * @param autenticacao O código numérico de autenticação do documento.
+     */
     public DocumentoAcademico(String criador, CodigoCurso codigoCurso, int paginas, long autenticacao) {
         super(criador, codigoCurso, paginas);
         this.autenticacao = autenticacao;
     }
 
+    /**
+     * Retorna o código de autenticação do documento.
+     *
+     * @return o código de autenticação.
+     */
     public long getAutenticacao() {
         return autenticacao;
     }
 
+    /**
+     * Compara este objeto DocumentoAcademico com outro objeto para verificar se
+     * são iguais.
+     *
+     * @param o O objeto a ser comparado.
+     * @return true se os objetos forem iguais, false caso contrário.
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) {
@@ -42,6 +67,11 @@ public class DocumentoAcademico extends Documento {
         return false;
     }
 
+    /**
+     * Retorna o código de hash para este objeto DocumentoAcademico.
+     *
+     * @return o código de hash.
+     */
     @Override
     public int hashCode() {
         return Objects.hash(super.hashCode(), this.autenticacao);
